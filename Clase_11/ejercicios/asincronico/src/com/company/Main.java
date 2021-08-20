b0dea5b249d19db008fdaaa47f2f9af8814dc1ad
dedosmedia <gerencia@dedosmedia.com>
2021-08-19 23:47:46 -0500

    Ejercicio Clase 11. Conexión por JDBC

--- Clase_11/ejercicios/asincronico/src/com/company/Main.java
+++ Clase_11/ejercicios/asincronico/src/com/company/Main.java
@@ -10,6 +10,21 @@ public class Main {
     public static void main(String[] args) throws Exception {
 	// write your code here
 
+        // Revisamos que dispongamos del Driver de H2
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (Exception e) {
+            System.out.println("Error no has vinculado el paquete h2"+e);;
+            System.exit(0);
+        }
+
+        // Creamos la conexión a la BD
+        Connection connection = DriverManager.getConnection("jdbc:h2:~/dh");
+
+        // Creamos el Statement que nos permitirá ejecutar consultas SQL
+        Statement stm = connection.createStatement();
+
+        // Ejecutamos la consulta para crear la tabla
         String SQL_CREATE_TABLE = "DROP TABLE IF EXISTS EMPLEADO; CREATE TABLE EMPLEADO "
                 + "("
                 + " ID INT PRIMARY KEY,"
@@ -17,25 +32,22 @@ public class Main {
                 + " EDAD INT NULL, "
                 + " EMPRESA varchar(100) NOT NULL "
                 + " )";
-
-        Connection connection = DriverManager.getConnection("jdbc:h2:~/dh");
-        Statement stm = connection.createStatement();
-
         stm.execute(SQL_CREATE_TABLE);
 
+        // Ejecutamos las consultas para insertar datos
         stm.execute("INSERT INTO EMPLEADO (ID, EMPRESA) VALUES (1, 'Digital')");
         stm.execute("INSERT INTO EMPLEADO (ID, EMPRESA) VALUES (2, 'Google')");
         stm.execute("INSERT INTO EMPLEADO (ID, EMPRESA) VALUES (3, 'Facebook')");
 
-
+        // Ejecutamos la consulta SELECT
         ResultSet rs = stm.executeQuery("SELECT * FROM EMPLEADO");
 
-
+        // Iteramos sobre los registros resultantes
         while( rs.next()) {
             System.out.println(rs.getInt(1) +rs.getString(4));
         }
 
+        // Cerrar la conexión a la BD
         connection.close();
-
     }
 }
--- Clase_11/ejercicios/sincronico/mesas/src/com/company/Main.java
+++ Clase_11/ejercicios/sincronico/mesas/src/com/company/Main.java
@@ -10,31 +10,44 @@ public class Main {
     public static void main(String[] args) throws Exception {
 	// write your code here
 
+        // Revisamos que dispongamos del Driver de H2
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (Exception e) {
+            System.out.println("Error no has vinculado el paquete h2"+e);;
+            System.exit(0);
+        }
+
+        // Realizamos la conexión a la BD
+        Connection connection = DriverManager.getConnection("jdbc:h2:~/dh");
+
+        // Creamos el Statement para ejecutar consultas SQL
+        Statement stm = connection.createStatement();
+
+        // Ejecutamos la consulta para crear tablas
         String SQL_CREATE_TABLE = "DROP TABLE IF EXISTS FORMAS; CREATE TABLE FORMAS "
                 + "("
                 + " ID INT PRIMARY KEY,"
                 + " FIGURA varchar(100) NOT NULL, "
                 + " COLOR varchar(100) NOT NULL "
                 + " )";
-
-        Connection connection = DriverManager.getConnection("jdbc:h2:~/dh");
-        Statement stm = connection.createStatement();
-
         stm.execute(SQL_CREATE_TABLE);
 
+        // Ejecutamos consultas parai nsertar datos
         stm.execute("INSERT INTO FORMAS (ID, FIGURA, COLOR) VALUES (1, 'Circulo', 'Rojo')");
         stm.execute("INSERT INTO FORMAS (ID, FIGURA, COLOR) VALUES (2, 'Circulo', 'Negro')");
         stm.execute("INSERT INTO FORMAS (ID, FIGURA, COLOR) VALUES (3, 'Cuadrado', 'Amarillo')");
         stm.execute("INSERT INTO FORMAS (ID, FIGURA, COLOR) VALUES (4, 'Cuadrado', 'Verde')");
 
-
+        // Ejecutamos consultas para consultar datos
         ResultSet rs = stm.executeQuery("SELECT * FROM FORMAS");
 
-
+        // Iteramos sobre los resultados
         while( rs.next()) {
             System.out.println(rs.getInt(1) +rs.getString(2) + rs.getString(3));
         }
 
+        // Cerramos la conexión
         connection.close();
 
     }
